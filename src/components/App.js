e02a2ae03af65799d7526b495d65e56f6cd24fd4
Arbiona <63557021+arb1ona@users.noreply.github.com>
2020-09-06 14:44:57 +0200

    Cleaner Code

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,6 @@ import Header from './Header';
 import MainContent from './MainContent';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div id="container">
